4794d4df7c950daeb4db69d35fa94436c5b3fedb
Omer Etrog <omer72@gmail.com>
2024-02-04 15:28:13 +0200

    add images for readme

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var path = require('path');
 var http = require('http');
 
 var oas3Tools = require('oas3-tools');
-var serverPort = 8080;
+var serverPort = process.env.PORT ||3000;
 
 // swaggerRouter configuration
 var options = {
